b8086f18fa94618794a2ad6c88f355e9922afbde
Kenton Waters <kenton.a.waters@gmail.com>
2025-12-28 06:00:48 -0600

    evaluate

--- src/util/tree-helpers.ts
+++ src/util/tree-helpers.ts
@@ -9,6 +9,125 @@ import type {
 import type ParseTree from "../models/lexing-parsing/parse-tree.js";
 import type { WhitespaceToken } from "../models/lexing-parsing/token.js";
 
+export const evaluate = (parseTree: ParseTree): number => {
+  const evaluateDiceRoll = (diceRoll: DiceRoll): number => {
+    const go = (numDice: number, numFaces: number): number => {
+      if (numDice <= 0) return 0;
+
+      return (
+        Math.floor(Math.random() * numFaces) + 1 + go(numDice - 1, numFaces)
+      );
+    };
+
+    const positiveSum = go(
+      diceRoll.numDice.numericValue,
+      diceRoll.positiveNumFacesToken.numericValue,
+    );
+
+    return diceRoll.sign.signValue === "-" ? -1 * positiveSum : positiveSum;
+  };
+
+  const evaluateAtom = (atom: Atom): number => {
+    switch (atom.tag) {
+      case "integer":
+        return atom.data.numericValue;
+      case "diceRoll":
+        return evaluateDiceRoll(atom.data);
+    }
+  };
+
+  const evaluateExpression = (expression: Expression): number => {
+    switch (expression.tag) {
+      case "atom":
+        return evaluateAtom(expression.data);
+      case "additionOrSubtraction":
+        return evaluateAdditionOrSubtraction(expression.data);
+    }
+  };
+
+  const evaluateAdditionOrSubtraction = (
+    additionOrSubtraction: AdditionOrSubtraction,
+  ): number => {
+    const leftSide: number = evaluateAtom(additionOrSubtraction.leftHandAtom);
+    const rightSide: number = evaluateExpression(
+      additionOrSubtraction.rightHandExpression,
+    );
+    return additionOrSubtraction.operatorToken.stringToken === "-"
+      ? leftSide - rightSide
+      : leftSide + rightSide;
+  };
+
+  const evaluateSingleAtom = (atom: Atom): number => {
+    switch (atom.tag) {
+      case "diceRoll":
+        return evaluateDiceRoll(atom.data);
+      case "integer":
+        // 1d20 + integer atom
+        return evaluateAdditionOrSubtraction({
+          leftHandAtom: {
+            tag: "diceRoll",
+            data: {
+              sign: {
+                signValue: "+",
+                signToken: null,
+                followingWhitespaceToken: null,
+              },
+              numDice: {
+                nonnegativeNumDiceToken: null,
+                numericValue: 1,
+                followingWhitespaceToken: null,
+              },
+              dieSymbol: {
+                dieToken: {
+                  stringToken: "d",
+                },
+                followingWhitespaceToken: null,
+              },
+              positiveNumFacesToken: {
+                numericValue: 20,
+                stringToken: "",
+              },
+              followingWhitespaceToken: null,
+            },
+          },
+          operatorToken: {
+            stringToken: "+",
+          },
+          rightHandExpression: {
+            tag: "atom",
+            data: atom,
+          },
+          followingWhitespaceToken: null,
+        });
+    }
+  };
+
+  switch (parseTree.expression?.tag) {
+    case "additionOrSubtraction":
+      return evaluateAdditionOrSubtraction(parseTree.expression.data);
+    case "atom":
+      return evaluateSingleAtom(parseTree.expression.data);
+    case undefined:
+      // 1d20 + 0
+      return evaluateSingleAtom({
+        tag: "integer",
+        data: {
+          sign: {
+            signValue: "+",
+            signToken: null,
+            followingWhitespaceToken: null,
+          },
+          numericValue: 0,
+          nonnegativeIntegerToken: {
+            stringToken: "",
+            numericValue: 0,
+          },
+          followingWhitespaceToken: null,
+        },
+      });
+  }
+};
+
 export const reconstructInputString = (parseTree: ParseTree): string => {
   const reconstructWhitespaceInputString = (
     whitespace: WhitespaceToken | null,
