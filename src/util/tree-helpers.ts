cb0dbd00ce9d094744ae9f0038c5f56deb061986
Kenton Waters <kenton.a.waters@gmail.com>
2025-12-28 04:55:51 -0600

    refactor reconstructInputString

--- src/util/tree-helpers.ts
+++ src/util/tree-helpers.ts
@@ -10,80 +10,82 @@ import type ParseTree from "../models/lexing-parsing/parse-tree.js";
 import type { WhitespaceToken } from "../models/lexing-parsing/token.js";
 
 export const reconstructInputString = (parseTree: ParseTree): string => {
+  const reconstructWhitespaceInputString = (
+    whitespace: WhitespaceToken | null,
+  ): string => {
+    return whitespace?.stringToken ?? "";
+  };
+
+  const reconstructExpressionInputString = (
+    expression: Expression | null,
+  ): string => {
+    if (expression === null) return "";
+
+    switch (expression.tag) {
+      case "additionOrSubtraction":
+        return reconstructAdditionOrSubtractionInputString(expression.data);
+      case "atom":
+        return reconstructAtomInputString(expression.data);
+    }
+  };
+
+  const reconstructAdditionOrSubtractionInputString = (
+    additionOrSubtraction: AdditionOrSubtraction,
+  ): string => {
+    return (
+      reconstructAtomInputString(additionOrSubtraction.leftHandAtom) +
+      additionOrSubtraction.operatorToken.stringToken +
+      reconstructWhitespaceInputString(
+        additionOrSubtraction.followingWhitespaceToken,
+      ) +
+      reconstructExpressionInputString(
+        additionOrSubtraction.rightHandExpression,
+      )
+    );
+  };
+
+  const reconstructAtomInputString = (atom: Atom): string => {
+    switch (atom.tag) {
+      case "integer":
+        return reconstructIntegerInputString(atom.data);
+      case "diceRoll":
+        return reconstructDiceRollInputString(atom.data);
+    }
+  };
+
+  const reconstructDiceRollInputString = (diceRoll: DiceRoll): string => {
+    return (
+      reconstructSignInputString(diceRoll.sign) +
+      (diceRoll.numDice.nonnegativeNumDiceToken?.stringToken ?? "") +
+      reconstructWhitespaceInputString(
+        diceRoll.numDice.followingWhitespaceToken,
+      ) +
+      diceRoll.dieSymbol.dieToken.stringToken +
+      reconstructWhitespaceInputString(
+        diceRoll.dieSymbol.followingWhitespaceToken,
+      ) +
+      diceRoll.positiveNumFacesToken.stringToken +
+      reconstructWhitespaceInputString(diceRoll.followingWhitespaceToken)
+    );
+  };
+
+  const reconstructIntegerInputString = (integer: Integer): string => {
+    return (
+      reconstructSignInputString(integer.sign) +
+      integer.nonnegativeIntegerToken.stringToken +
+      reconstructWhitespaceInputString(integer.followingWhitespaceToken)
+    );
+  };
+
+  const reconstructSignInputString = (sign: Sign): string => {
+    return (
+      (sign.signToken?.stringToken ?? "") +
+      reconstructWhitespaceInputString(sign.followingWhitespaceToken)
+    );
+  };
+
   return (
     reconstructWhitespaceInputString(parseTree.initialWhitespaceToken) +
     reconstructExpressionInputString(parseTree.expression)
-  );
-};
-
-const reconstructWhitespaceInputString = (
-  whitespace: WhitespaceToken | null,
-): string => {
-  return whitespace?.stringToken ?? "";
-};
-
-const reconstructExpressionInputString = (
-  expression: Expression | null,
-): string => {
-  if (expression === null) return "";
-
-  switch (expression.tag) {
-    case "additionOrSubtraction":
-      return reconstructAdditionOrSubtractionInputString(expression.data);
-    case "atom":
-      return reconstructAtomInputString(expression.data);
-  }
-};
-
-const reconstructAdditionOrSubtractionInputString = (
-  additionOrSubtraction: AdditionOrSubtraction,
-): string => {
-  return (
-    reconstructAtomInputString(additionOrSubtraction.leftHandAtom) +
-    additionOrSubtraction.operatorToken.stringToken +
-    reconstructWhitespaceInputString(
-      additionOrSubtraction.followingWhitespaceToken,
-    ) +
-    reconstructExpressionInputString(additionOrSubtraction.rightHandExpression)
-  );
-};
-
-const reconstructAtomInputString = (atom: Atom): string => {
-  switch (atom.tag) {
-    case "integer":
-      return reconstructIntegerInputString(atom.data);
-    case "diceRoll":
-      return reconstructDiceRollInputString(atom.data);
-  }
-};
-
-const reconstructDiceRollInputString = (diceRoll: DiceRoll): string => {
-  return (
-    reconstructSignInputString(diceRoll.sign) +
-    (diceRoll.numDice.nonnegativeNumDiceToken?.stringToken ?? "") +
-    reconstructWhitespaceInputString(
-      diceRoll.numDice.followingWhitespaceToken,
-    ) +
-    diceRoll.dieSymbol.dieToken.stringToken +
-    reconstructWhitespaceInputString(
-      diceRoll.dieSymbol.followingWhitespaceToken,
-    ) +
-    diceRoll.positiveNumFacesToken.stringToken +
-    reconstructWhitespaceInputString(diceRoll.followingWhitespaceToken)
-  );
-};
-
-const reconstructIntegerInputString = (integer: Integer): string => {
-  return (
-    reconstructSignInputString(integer.sign) +
-    integer.nonnegativeIntegerToken.stringToken +
-    reconstructWhitespaceInputString(integer.followingWhitespaceToken)
-  );
-};
-
-const reconstructSignInputString = (sign: Sign): string => {
-  return (
-    (sign.signToken?.stringToken ?? "") +
-    reconstructWhitespaceInputString(sign.followingWhitespaceToken)
   );
 };
