bd51d308b045ae14b23eaa9b4c42793d09d46050
Kenton Waters <kenton@boundlesswaters.com>
2026-03-05 20:55:19 -0600

    null

--- src/models/generic/tagged.ts
+++ src/models/generic/tagged.ts
@@ -2,6 +2,8 @@ type Tagged<TagType, ContentType = undefined> = {
   readonly tag: TagType; // All instances will have the tag property
 } & ([ContentType] extends [undefined]
   ? object // We will not add any properties for an undefined ContentType
-  : { readonly tag: TagType; readonly data: ContentType });
+  : [ContentType] extends [null]
+    ? { readonly null: ContentType }
+    : { readonly tag: TagType; readonly data: ContentType });
 
 export type { Tagged as default };
