e6d2399cf2a103bb6287a3cde76e0c1fea14284e
Kenton Waters <kenton@boundlesswaters.com>
2026-03-05 21:33:06 -0600

    object with tag type

--- src/models/generic/tagged.ts
+++ src/models/generic/tagged.ts
@@ -1,15 +1,19 @@
 type Tagged<TagType, ContentType = undefined> = {
   readonly tag: TagType; // All instances will have the tag property
-} & ([ContentType] extends [undefined]
-  ? object // We will not add any properties for an undefined ContentType
-  : [ContentType] extends [null]
-    ? { readonly null: ContentType }
-    : [ContentType] extends [string]
-      ? { readonly string: ContentType }
-      : [ContentType] extends [number]
-        ? { readonly number: ContentType }
-        : [ContentType] extends [boolean]
-          ? { readonly boolean: ContentType }
-          : { readonly tag: TagType; readonly data: ContentType });
+} & ([ContentType] extends [undefined | null]
+  ? object // We will not add any properties for an undefined or null ContentType
+  : [ContentType] extends [string]
+    ? { readonly string: ContentType }
+    : [ContentType] extends [number]
+      ? { readonly number: ContentType }
+      : [ContentType] extends [boolean]
+        ? { readonly boolean: ContentType }
+        : [ContentType] extends [bigint]
+          ? { readonly bigint: ContentType }
+          : [ContentType] extends [symbol]
+            ? { readonly symbol: ContentType }
+            : "tag" extends keyof [ContentType]
+              ? { readonly content: ContentType }
+              : { readonly data: ContentType });
 
 export type { Tagged as default };
