5704ee1814077956e4d519bef4f166d1522ab095
Kenton Waters <kenton@boundlesswaters.com>
2026-03-19 12:42:28 -0500

    add docker init support

--- flake.nix
+++ flake.nix
@@ -8,13 +8,15 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in
     {
       devShells.${system}.default = pkgs.mkShellNoCC {
         buildInputs = with pkgs; [
           nodejs_25
-          docker
+          (docker.override {
+            initSupport = true;
+          })
         ];
 
         shellHook = ''
